c3a13dcb071681f7635a2a677bed4fd3e07b098d
Marek Pyczkowski & Ravi <pair+marekpwk.ravip0711@devbootcamp.com>
2014-08-27 16:58:31 -0500

    created rspec associations for cart, category, and product models

--- amozin/spec/models/product_spec.rb
+++ amozin/spec/models/product_spec.rb
@@ -1,8 +1,20 @@
-Products
-- view a product, so that I can decide to buy it or not
-  - view the product's name
-  - view the product's category
-  - view the product's price
-  - view the product's description
-  - view the product's image
-- view similar products that have high reviews (products in the same category that are not selected), so that I can see
+require 'rails_helper'
+
+describe Product do
+
+  it { should have_many(:reviews) }
+
+  it { should have_many(:item_to_buys) }
+
+  it { should belong_to(:category) }
+end
+
+
+# Products
+# - view a product, so that I can decide to buy it or not
+#   - view the product's name
+#   - view the product's category
+#   - view the product's price
+#   - view the product's description
+#   - view the product's image
+# - view similar products that have high reviews (products in the same category that are not selected), so that I can see

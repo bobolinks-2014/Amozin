24e3ffb1fcf9665f555fa70467de29022b717fea
Casey Ryan & Rob Schwartz <pair+cjryan28.robschwartz@devbootcamp.com>
2014-08-27 16:14:52 -0500

    Shoulda matchers gem installed so that we can do testing with active model

--- amozin/spec/models/user_spec.rb
+++ amozin/spec/models/user_spec.rb
@@ -1,3 +1,32 @@
 Users
 - edit my profile, so that I can keep my information current
 - delete my profile, so that I can disassociate myself from the site
+
+
+describe 'User' do
+
+    let(:user) { User.create(
+                {username: "create tests",
+                 email: "use rspec to drive development",
+                 password: })
+  }
+
+  it 'has many reviews' do
+    expect(user.reviews).to not_be_nil
+  end
+
+  it 'can only be created with a password' do
+    expect(user )
+  end
+
+  it 'can be created with an email in a certain format' do
+  end
+
+  it 'can edit his profile, so that I can keep his information current' do
+      expect
+  end
+
+  it 'can delete his profile, so that he can dissassociate himself from the site' do
+  end
+
+end

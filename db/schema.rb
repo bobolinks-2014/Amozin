13a31f9a44afac2fb3527bd193b3af03af28bb7b
Casey Ryan & Rob Schwartz <pair+cjryan28.robschwartz@devbootcamp.com>
2014-08-28 11:01:31 -0500

    updated routes

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20140827200253) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"

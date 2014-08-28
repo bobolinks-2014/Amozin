aa57a3a3d4f59b74068b0f3bbe21067449ecbfd5
Marek Pyczkowski & Ravi <pair+marekpwk.ravip0711@devbootcamp.com>
2014-08-28 18:56:47 -0500

    sessions up and running, errors pop up if dont meet validations. redirects work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # attr_accessible :email, :password, :password_confirmation
+
   has_many :reviews
   has_secure_password
   validates :password, :length => { :minimum => 6 }

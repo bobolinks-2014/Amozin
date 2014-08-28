25955ef8dfddbc8a15ae3e21851773395be6a672
Casey Ryan & Rob Schwartz <pair+cjryan28.robschwartz@devbootcamp.com>
2014-08-28 10:38:47 -0500

    removed amozin folder

--- amozin/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :reviews
-  has_secure_password
-  validates :password, :length => { :minimum => 6 }
-  validates :email, uniqueness: true, :format => /.+@.+\..+/
-end

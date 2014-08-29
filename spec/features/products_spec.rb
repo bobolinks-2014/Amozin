81316a1896d568839bbc8fe0908019f7dedc1864
Casey Ryan & Rob Schwartz <pair+cjryan28.robschwartz@devbootcamp.com>
2014-08-29 09:20:22 -0500

    updated path for tests

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 feature 'User browsing the website' do
 
 
-  context 'on category page' do
+  context 'on category\'s products page' do
     it 'sees a list of products' do
       category = Category.create(name: 'Snippets')
       Product.create( name: 'Meatball Skateboard',
                       price: 2,
                       description: 'Greate way to get around town while staying off the hangry train',
                       category_id: category.id)
-      visit category_path(category)
+      visit category_products_path(category)
       expect(page).to have_content "Meatball Skateboard"
     end
 
@@ -20,7 +20,7 @@ feature 'User browsing the website' do
                       price: 2,
                       description: 'Greate way to get around town while staying off the hangry train',
                       category_id: category.id)
-      visit category_path(category)
+      visit category_products_path(category)
       expect(page).to have_content "Snippets"
     end
 
@@ -30,7 +30,7 @@ feature 'User browsing the website' do
                       price: 2,
                       description: 'Greate way to get around town while staying off the hangry train',
                       category_id: category.id)
-      visit category_path(category)
+      visit category_products_path(category)
       expect(page).to have_content "2"
     end
 
@@ -40,7 +40,7 @@ feature 'User browsing the website' do
                       price: 2,
                       description: 'Great way to get around town while staying off the hangry train',
                       category_id: category.id)
-      visit category_path(category)
+      visit category_products_path(category)
       expect(page).to have_content "Great way to get around town while staying off the hangry train"
     end
 
@@ -51,26 +51,26 @@ feature 'User browsing the website' do
                       image_url: "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg",
                       description: 'Great way to get around town while staying off the hangry train',
                       category_id: category.id)
-      visit category_path(category)
+      visit category_products_path(category)
       expect(page).to have_content "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg"
     end
 
-    # it 'sees a list of similar products' do
-    #   category = Category.create(name: 'Snippets')
-    #   Product.create( name: 'Meatball Skateboard',
-    #                   price: 2,
-    #                   image_url: "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg",
-    #                   description: 'Great way to get around town while staying off the hangry train',
-    #                   category_id: category.id)
-    #   product = Product.create( name: 'Canaries in Cantaloupes',
-    #                   price: 3.8,
-    #                   image_url: "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg",
-    #                   description: 'Great way to get around town while staying off the hangry train',
-    #                   category_id: category.id)
-    #   visit product_path(product)
-    #   expect(page).to have_content 'Meatball Skateboard'
-    #
-    # end
+    it 'sees a list of similar products' do
+      category = Category.create(name: 'Snippets')
+      Product.create( name: 'Meatball Skateboard',
+                      price: 2,
+                      image_url: "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg",
+                      description: 'Great way to get around town while staying off the hangry train',
+                      category_id: category.id)
+      product = Product.create( name: 'Canaries in Cantaloupes',
+                      price: 3.8,
+                      image_url: "http://cdn0.lostateminor.com/wp-content/uploads/2012/01/funny-fake-products2.jpg",
+                      description: 'Great way to get around town while staying off the hangry train',
+                      category_id: category.id)
+      visit category_product_path(product)
+      expect(page).to have_content 'Meatball Skateboard'
+
+    end
   end
 
   context 'Reviews options' do

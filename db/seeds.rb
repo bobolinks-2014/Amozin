9d5439499cc85ce724d40ced05796704653b6aaa
Marek Pyczkowski & Ravi <pair+marekpwk.ravip0711@devbootcamp.com>
2014-08-29 18:04:13 -0500

    added carousel for pics feel free to add on

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,49 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-food = Category.new(name: "Flexible Food")
+food = Category.create(name: "Flexible Food")
 
-Product.new(name: "Squeeze Bacon",
-			description: "Direct from Sweden, the bacon you can squeeze comes in a ketchup-style bottle from which you can squeeze out strips of delicious bacon onto any meal. According to ThinkGeek.com, Squeez Bacon was invented by a young Swedish who found that “by precooking it and blending it in a special way, he was able to make a fully cooked 100% bacon paste that could be squeezed through a  tube.” All we can say is WOW. Why was this not invented sooner?",
+Product.create(name: "Squeeze Bacon",
+			description: "The bacon you can squeeze comes in a ketchup-style bottle from which you can squeeze out strips of delicious bacon onto any meal.",
 			price: 8,
-			image_url: "https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/bacon.jpeg",
+			image_url: "bacon.jpeg",
 			category_id: food.id)
 
-Product.new(name: "Cheesy Poofs",
+Product.create(name: "Cheesy Poofs",
 			description:"Any puffed grain snack which is flavored like cheese, such as Cheet-o's, but in particular various generic 'Cheese Puffs' which are round and.. poofy.
 				(as a matter of fact, my sister used this term a lot before southpark was launched.)",
 			price: 3.50,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/cheesy_poofs.jpeg",
+			image_url:"cheesy_poofs.jpeg",
 			category_id: food.id)
 
-Product.new(name:"Duff Beer",
+Product.create(name:"Duff Beer",
 			description:"stereotypical commercial beer: cheap, poor-quality, and advertised everywhere.",
 			price: 5,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/duff.jpeg",
+			image_url:"duff.jpeg",
 			category_id: food.id)
 
-Product.new(name:"Nuts and Gum",
+Product.create(name:"Nuts and Gum",
 			description:"Together at last!",
 			price: 2,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/gum.jpeg",
+			image_url:"gum.jpeg",
 			category_id: food.id)
 
-Product.new(name: "Krabby Patty",
+Product.create(name: "Krabby Patty",
 			description:"A Krabby Patty is a burger sold at the Krusty Krab. It is the best-known food at the Krusty Krab, and the most famous sandwich in Bikini Bottom.",
 			price: 6,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/krabby.jpeg",
+			image_url:"krabby.jpeg",
 			category_id: food.id)
 
-Product.new(name: "Stay Puft",
+Product.create(name: "Stay Puft",
 			description: "Formerly famous. Now legendary. Also, I am a marshmallow.",
 			price: 4,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/puffs.jpeg",
+			image_url:"puffs.jpeg",
 			category_id: food.id)
 
-Product.new(name: "Unicorn Meat",
+Product.create(name: "Unicorn Meat",
 			description:"Unicorns, as we all know, frolic all over the world, pooping rainbows and marshmallows wherever they go. We are confident you will find a world of bewilderment in every mouthful of scrumptious unicorn meat.",
 			price: 100,
-			image_url:"https://raw.githubusercontent.com/bobolinks-2014/Amozin/seed/images/unicorn.jpeg",
+			image_url:"unicorn.jpeg",
 			category_id: food.id)
 
 

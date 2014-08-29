92492901088de7fcac291e57fbe38489bb44a400
robschwartz <robertschwar@gmail.com>
2014-08-28 21:33:52 -0500

    added slugs to products and categories. Also added friendlyID migration table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :products
+
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :history] 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
   has_many :reviews
   has_many :item_to_buys
   belongs_to :category
+
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :history] 
 end

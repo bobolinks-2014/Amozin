33d734945f43c0c1b3d709ca6e4d9d67337b48c7
Casey Ryan & Rob Schwartz <pair+cjryan28.robschwartz@devbootcamp.com>
2014-08-30 17:14:52 -0500

    added css for homepage

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,12 @@ class Product < ActiveRecord::Base
   has_many :item_to_buys
   belongs_to :category
 
-
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
+
+  def self.featured_products
+    self.all.sample(4)
+  end
 
   def show_all_other_products
     Product.where(category_id: self.category_id).to_a.delete_if{|product| product.id == self.id}

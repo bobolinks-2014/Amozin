abb0bcb0db7f363dcdba9c84ecc20c214a0d62b6
Marek <rkx1901@gmail.com>
2014-08-30 17:48:32 -0500

    created a buy button in the shopping cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,21 @@
 class CartsController < ApplicationController
   def show
-
   end
 
   def update
-
   end
 
   def destroy
     cart = Cart.find(params[:id])
     cart.item_to_buys.destroy_all
       redirect_to cart_path(current_cart)
+  end
+
+  def create
+    cart = current_cart
+    cart.item_to_buys.destroy_all
+    flash[:notice] = "Congratulations, Your Purchase Was Completed, too bad this is a fake website!"
+    redirect_to cart_path(current_cart)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Amozin::Application.routes.draw do
   resources :users
   get "signup" => "users#new", :as => "signup"
   resources :sessions
-  resources :carts, only: [:show, :update, :destroy]
+  resources :carts, only: [:show, :update, :destroy, :create]
 # post '/login' => 'sessions#new', as: :login
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

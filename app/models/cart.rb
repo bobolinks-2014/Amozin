23e4261d59735d3fbc21bd0ac902276453b3c670
Casey Ryan <cjryan28@gmail.com>
2014-08-31 17:00:27 -0500

    similar products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -13,7 +13,34 @@ class Cart < ActiveRecord::Base
       current_item
   end
 
+
+  def similar_products
+
+    my_products = self.item_to_buys.pluck("product_id")
+
+    all_carts_products = []
+    Cart.all.each do |cart|
+      all_carts_products << cart.item_to_buys.pluck("product_id")
+    end
+
+    possible_carts_products = []
+    all_carts_products.each do |cart|
+      if (my_products & cart) == my_products
+        possible_carts_products << cart
+      end
+    end
+
+    similar_products = possible_carts_products.flatten - my_products
+
+    similar_products.map!{ |product_id| Product.find(product_id) }
+
+    similar_products
+
+  end
+
   def total_price
     item_to_buys.to_a.sum { |item| item.total_price }
   end
 end
+
+

71d9e00899fbca6dd16c21151f7573c82a2fc00d
Marek <rkx1901@gmail.com>
2014-08-30 20:12:19 -0500

    put rating stars on the unique product's page
    fixed reviews to display at the bottom of the page
    fixed product quantity to update when customer buys the products

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,7 +15,10 @@ class CartsController < ApplicationController
     cart = current_cart
     current_cart.item_to_buys.each do |item|
     item.product.quantity = item.product.quantity - item.quantity
+    item.product.save
+     # sdfsd
     end
+
     cart.item_to_buys.destroy_all
     flash[:notice] = "Congratulations, Your Purchase Was Completed, too bad this is a fake website!"
     redirect_to cart_path(current_cart)
